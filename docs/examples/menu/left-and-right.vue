fa21c592b362396b72b0262ff50fe88f3db30b09
zale123456 <2356488390@qq.com>
2024-10-31 20:42:42 +0800

    Update left-and-right.vue

--- docs/examples/menu/left-and-right.vue
+++ docs/examples/menu/left-and-right.vue
@@ -16,7 +16,7 @@
     <el-menu-item index="1">Processing Center</el-menu-item>
     <el-sub-menu index="2">
       <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-1">item aaaaa</el-menu-item>
       <el-menu-item index="2-2">item two</el-menu-item>
       <el-menu-item index="2-3">item three</el-menu-item>
       <el-sub-menu index="2-4">
